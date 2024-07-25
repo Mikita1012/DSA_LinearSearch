76e6a913ff858a39e9552cb51f764514da028d45
Mikita.Shah <mikitashah2009@gmail.com>
2024-07-25 12:51:33 +0530

    added an optimized function as digits2 in the code

--- src/EvenNumOfDigits.java
+++ src/EvenNumOfDigits.java
@@ -3,6 +3,9 @@ public class EvenNumOfDigits {
         int[] arr = {124, 89, 90, 1231, 421344, 7654, 21111};
        int res =  findNumbers(arr);
         System.out.println("No of even digits in array are: "+res);
+
+        System.out.println(digits2(1234));
+
     }
 
     static int findNumbers(int[] arr) {
@@ -21,13 +24,30 @@ public class EvenNumOfDigits {
         return noOfDigits % 2 == 0;
     }
 
-//    count number of digites in a number
+//    count number of digits in a number
     static int digits(int num) {
         int count =0;
+
+        if(num < 0) {
+            num = num * -1;
+        }
+        if(num == 0) {
+            return 1;
+        }
         while(num > 0) {
             count++;
             num = num / 10;
         }
         return count;
+    }
+
+    static int digits2(int num) {
+        if (num < 0) {
+            num = num * -1;
+        } else if (num == 0) {
+            return 1;
+        }
+
+        return (int)(Math.log10(num)) + 1;
     }
 }
